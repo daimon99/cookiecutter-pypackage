06981dc039d4a85d6ebb0e4089c812dc45b2fa15
Jian Dai <daijian1@qq.com>
2018-12-12 00:09:28 +0800

    Update test_{{cookiecutter.project_slug}}.py

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -4,9 +4,7 @@
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 
 import pytest
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
 from click.testing import CliRunner
-{%- endif %}
 
 from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
@@ -28,8 +26,6 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-
 
 def test_command_line_interface():
     """Test the CLI."""
@@ -40,4 +36,3 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
-{%- endif %}
